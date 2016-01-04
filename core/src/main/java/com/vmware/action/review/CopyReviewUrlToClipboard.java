878fbcb6860344b78aa226ffac6bfb9e767b6f03
Damien Biggs <dbiggs@vmware.com>
2016-01-04 11:27:57 -0500

    ReviewUrl: Added command line approach for copying url
    Prevents jumping back to desktop behavior on OSX.
    Uses pbcopy command line tool on OSX.

--- core/src/main/java/com/vmware/action/review/CopyReviewUrlToClipboard.java
+++ core/src/main/java/com/vmware/action/review/CopyReviewUrlToClipboard.java
@@ -1,9 +1,11 @@
 package com.vmware.action.review;
 
+import com.vmware.ServiceLocator;
 import com.vmware.action.base.AbstractCommitWithReviewAction;
 import com.vmware.config.ActionDescription;
 import com.vmware.config.WorkflowConfig;
 import com.vmware.http.UrlUtils;
+import com.vmware.utils.IOUtils;
 
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
@@ -11,6 +13,7 @@ import java.awt.datatransfer.StringSelection;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.text.ParseException;
+import java.util.Locale;
 
 @ActionDescription("Copies the review board url to the clipboard. Handy for pasting it into a browser.")
 public class CopyReviewUrlToClipboard extends AbstractCommitWithReviewAction {
@@ -19,11 +22,34 @@ public class CopyReviewUrlToClipboard extends AbstractCommitWithReviewAction {
     }
 
     @Override
+    public void preprocess() throws IOException, URISyntaxException, IllegalAccessException {
+        // don't need to load reviewboard for this action
+    }
+
+
+    @Override
     public void process() throws IOException, IllegalAccessException, URISyntaxException, ParseException {
         String reviewUrl = String.format("%sr/%s/", UrlUtils.addTrailingSlash(config.reviewboardUrl), draft.id);
-        StringSelection stringSelection = new StringSelection (reviewUrl);
-        Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard ();
-        clpbrd.setContents (stringSelection, null);
+
+        String osName = System.getProperty("os.name").toLowerCase(Locale.ENGLISH);
+        boolean isOsx = osName.contains("mac") || osName.contains("darwin");
+
+        if (isOsx) {
+            copyUsingPbcopyCommand(reviewUrl);
+        } else {
+            StringSelection stringSelection = new StringSelection(reviewUrl);
+            Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
+            clpbrd.setContents(stringSelection, null);
+        }
         log.info("Copied review url to clipboard");
+    }
+
+    private void copyUsingPbcopyCommand(String reviewUrl) throws IOException {
+        log.debug("Using pbcopy command to copy review url to clipboard as it doesn't cause terminal in full screen mode to jump back to the desktop view");
+        ProcessBuilder builder = new ProcessBuilder("pbcopy").redirectErrorStream(true);
+        Process statusProcess = builder.start();
+        IOUtils.write(statusProcess.getOutputStream(), reviewUrl);
+        String output = IOUtils.read(statusProcess.getInputStream());
+        log.debug(output);
     }
 }
