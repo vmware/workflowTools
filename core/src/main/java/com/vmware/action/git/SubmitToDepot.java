db9d1b2a3e69f138fe43cf1e19202d62df16bd86
Damien Biggs <dbiggs@vmware.com>
2016-06-03 19:52:37 -0400

    Logging: Add more logging for submitting a change
    More logging for git p4 submit
    
    
    Former-commit-id: 9b447ea8116e19631e624cb0fccf6ffb4c1a5e77

--- core/src/main/java/com/vmware/action/git/SubmitToDepot.java
+++ core/src/main/java/com/vmware/action/git/SubmitToDepot.java
@@ -3,6 +3,7 @@ package com.vmware.action.git;
 import com.vmware.action.BaseAction;
 import com.vmware.config.ActionDescription;
 import com.vmware.config.WorkflowConfig;
+import com.vmware.util.StringUtils;
 
 @ActionDescription("Uses git p4 submit to checkin a commit to the perforce depot")
 public class SubmitToDepot extends BaseAction {
@@ -12,6 +13,9 @@ public class SubmitToDepot extends BaseAction {
 
     @Override
     public void process() {
+        if (StringUtils.isBlank(git.configValue("git-p4.skipsubmitedit"))) {
+            log.info("Run git command [git config git-p4.skipsubmitedit true] to skip opening a text editor while submitting");
+        }
         git.submit();
     }
 }
