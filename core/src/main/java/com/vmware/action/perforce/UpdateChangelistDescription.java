2277247f6027878206abe86720eee44b1fbf9148
Damien Biggs <dbiggs@vmware.com>
2016-06-29 15:36:34 -0400

    GitChangeset: Removed HardSubmitReview action
    Removed action from submitWithGitChangeset workflow.
    Easier to configure hard and soft submitting at the project level.
    
    
    Former-commit-id: abd54d876833cc616b2414b77afa1d383a581f12

--- core/src/main/java/com/vmware/action/perforce/UpdateChangelistDescription.java
+++ core/src/main/java/com/vmware/action/perforce/UpdateChangelistDescription.java
@@ -3,6 +3,7 @@ package com.vmware.action.perforce;
 import com.vmware.action.base.BasePerforceCommitAction;
 import com.vmware.config.ActionDescription;
 import com.vmware.config.WorkflowConfig;
+import com.vmware.util.StringUtils;
 
 @ActionDescription("Updates the description for the pending changelist.")
 public class UpdateChangelistDescription extends BasePerforceCommitAction {
@@ -14,6 +15,14 @@ public class UpdateChangelistDescription extends BasePerforceCommitAction {
     public void process() {
         String description = draft.toGitText(config.getCommitConfiguration());
         String existingDescription = perforce.readChangelist(draft.perforceChangelistId);
+        if (StringUtils.isBlank(existingDescription)) {
+            log.error("No description read for changelist {}", draft.perforceChangelistId);
+            return;
+        } else if (!existingDescription.contains("\n")) {
+            log.error("Not updating description for changelist {} as it has no new line characters\n{}",
+                    draft.perforceChangelistId, existingDescription);
+            return;
+        }
         existingDescription = existingDescription.substring(existingDescription.indexOf('\n')).trim();
         if (description.equals(existingDescription)) {
             log.info("Not updating description for changelist {} as it hasn't changed", draft.perforceChangelistId);
