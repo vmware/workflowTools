d9a389101abe42689ade240270a28a87481480fd
Damien Biggs <dbiggs@vmware.com>
2017-01-09 15:13:03 -0500

    Release: Check in new jar for patch fixes in recent commits.
    .
    
    
    Former-commit-id: 26e6a26396f51dd7d96047c24193a0696e7b2cbd

--- core/src/main/java/com/vmware/action/perforce/SelectExistingChangelist.java
+++ core/src/main/java/com/vmware/action/perforce/SelectExistingChangelist.java
@@ -6,10 +6,11 @@ import com.vmware.config.ActionDescription;
 import com.vmware.config.WorkflowConfig;
 import com.vmware.reviewboard.domain.ReviewRequestDraft;
 import com.vmware.util.StringUtils;
+import com.vmware.util.input.InputUtils;
 
 import java.util.List;
 
-@ActionDescription("Selects the existing changelist if only one changelist exists.")
+@ActionDescription("If only one changelist exists, that is selected by default. Otherwise the user is asked to select a changelist.")
 public class SelectExistingChangelist extends BasePerforceCommitAction {
     public SelectExistingChangelist(WorkflowConfig config) {
         super(config);
@@ -31,9 +32,16 @@ public class SelectExistingChangelist extends BasePerforceCommitAction {
     public void process() {
         List<String> changelists = perforce.getPendingChangelists();
 
+        if (changelists.isEmpty()) {
+            log.info("No pending changelists in client {}", perforce.getClientName());
+            return;
+        }
+
         if (changelists.size() == 1) {
             log.info("Using changelist {} as matching changelist since only one changelist exists", changelists.get(0));
             draft.perforceChangelistId = changelists.get(0);
+        } else {
+            draft.perforceChangelistId = InputUtils.readValueUntilNotBlank("Select changelist to use", changelists);
         }
     }
 }
