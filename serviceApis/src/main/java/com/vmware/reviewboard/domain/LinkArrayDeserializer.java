dda86afcff60412b8823cc016212603b028c9dcf
Damien Biggs <dbiggs@vmware.com>
2017-03-01 15:27:26 -0500

    Json: Rename json mappers to deserializers
    Makes it more obvious that they are just used for deserialization.

--- serviceApis/src/main/java/com/vmware/reviewboard/domain/LinkArrayDeserializer.java
+++ serviceApis/src/main/java/com/vmware/reviewboard/domain/LinkArrayDeserializer.java
@@ -4,12 +4,10 @@ import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
 
 import java.lang.reflect.Type;
 
-public class LinkArrayMapper implements JsonSerializer<String>, JsonDeserializer<String> {
+public class LinkArrayDeserializer implements JsonDeserializer<String> {
 
     @Override
     public String deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
@@ -25,10 +23,5 @@ public class LinkArrayMapper implements JsonSerializer<String>, JsonDeserializer
             groupNames += link.getTitle();
         }
         return groupNames;
-    }
-
-    @Override
-    public JsonElement serialize(String s, Type type, JsonSerializationContext jsonSerializationContext) {
-        return jsonSerializationContext.serialize(s, type);
     }
 }
