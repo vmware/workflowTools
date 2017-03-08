3ce893ed7ba2a135f6fea79585da0ec13d7034d7
Damien Biggs <dbiggs@vmware.com>
2017-03-08 18:14:47 -0500

    Perforce: Skip git changeset related actions if not in git directory
    Prevent the actions throwing exceptions.

--- core/src/main/java/com/vmware/action/gitchangeset/UpdateChangelistWithGitChangeset.java
+++ core/src/main/java/com/vmware/action/gitchangeset/UpdateChangelistWithGitChangeset.java
@@ -1,27 +1,22 @@
 package com.vmware.action.gitchangeset;
 
-import com.vmware.action.base.BasePerforceCommitAction;
+import com.vmware.action.base.BaseLinkedPerforceCommitAction;
 import com.vmware.config.ActionDescription;
 import com.vmware.config.WorkflowConfig;
 import com.vmware.util.StringUtils;
 import com.vmware.util.logging.LogLevel;
 
 @ActionDescription("Updates the git tag first, then runs git changeset update. Git changeset is a vmware internal tool.")
-public class UpdateChangelistWithGitChangeset extends BasePerforceCommitAction {
+public class UpdateChangelistWithGitChangeset extends BaseLinkedPerforceCommitAction {
     public UpdateChangelistWithGitChangeset(WorkflowConfig config) {
         super(config);
     }
 
     @Override
-    public String failWorkflowIfConditionNotMet() {
-        if (StringUtils.isBlank(git.configValue("changesetsync.checkoutdir"))) {
-            return "git changeset has not been setup for this repo, git config value changesetsync.checkoutdir is missing";
-        }
-        return super.failWorkflowIfConditionNotMet();
-    }
-
-    @Override
     public void process() {
+        if (StringUtils.isBlank(git.configValue("changesetsync.checkoutdir"))) {
+            throw new RuntimeException("git changeset has not been setup for this repo, git config value changesetsync.checkoutdir is missing");
+        }
         git.updateTag("changeset-" + draft.perforceChangelistId, LogLevel.DEBUG);
         git.changesetCommand("update", LogLevel.INFO);
     }
