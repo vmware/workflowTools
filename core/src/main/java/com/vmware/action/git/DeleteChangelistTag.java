52e1ffffdf85dd45111d3e09aab33a2ea17f6eb0
Damien Biggs <dbiggs@vmware.com>
2017-03-08 18:14:47 -0500

    Perforce: Skip git changeset related actions if not in git directory
    Prevent the actions throwing exceptions.
    
    
    Former-commit-id: 3ce893ed7ba2a135f6fea79585da0ec13d7034d7

--- core/src/main/java/com/vmware/action/git/DeleteChangelistTag.java
+++ core/src/main/java/com/vmware/action/git/DeleteChangelistTag.java
@@ -4,17 +4,22 @@ import com.vmware.action.base.BaseLinkedPerforceCommitAction;
 import com.vmware.action.base.BasePerforceCommitAction;
 import com.vmware.config.ActionDescription;
 import com.vmware.config.WorkflowConfig;
+import com.vmware.util.StringUtils;
 
 @ActionDescription("Deletes matching changelist tag.")
 public class DeleteChangelistTag extends BaseLinkedPerforceCommitAction {
 
     @Override
     public String cannotRunAction() {
+        String actionCannotBeRun = super.cannotRunAction();
+        if (StringUtils.isNotBlank(actionCannotBeRun)) {
+            return actionCannotBeRun;
+        }
         String expectedTagName = "changeset-" + draft.perforceChangelistId;
         if (!git.listTags().contains(expectedTagName)) {
             return "tag " + expectedTagName + " does not exist";
         }
-        return super.cannotRunAction();
+        return null;
     }
 
     public DeleteChangelistTag(WorkflowConfig config) {
