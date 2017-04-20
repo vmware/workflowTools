fee0eea4f079a8953da5c1f54a7fbaaa02f71b01
Damien Biggs <dbiggs@vmware.com>
2017-04-20 19:46:16 -0400

    Perforce: Check for text nothing changed when reopening files
    Reopening of some files can result in the text of nothing changed
    Added action for updating git tag for changelist if they are out of sync.

---
+++ core/src/main/java/com/vmware/action/git/UpdateChangelistTag.java
@@ -0,0 +1,36 @@
+package com.vmware.action.git;
+
+import com.vmware.action.base.BaseLinkedPerforceCommitUsingGitAction;
+import com.vmware.config.ActionDescription;
+import com.vmware.config.WorkflowConfig;
+import com.vmware.util.StringUtils;
+import com.vmware.util.logging.LogLevel;
+
+import java.util.List;
+
+@ActionDescription("Updates matching changelist tag if not set or revision is different.")
+public class UpdateChangelistTag extends BaseLinkedPerforceCommitUsingGitAction {
+
+    public UpdateChangelistTag(WorkflowConfig config) {
+        super(config);
+    }
+
+    @Override
+    public void process() {
+        String expectedTagName = "changeset-" + draft.perforceChangelistId;
+        List<String> existingTags = git.listTags();
+        if (!existingTags.contains(expectedTagName)) {
+            log.info("Creating tag {}", expectedTagName);
+            git.updateTag(expectedTagName, LogLevel.INFO);
+        } else {
+            String existingRevision = git.revParse(expectedTagName);
+            String headRevision = git.revParse("head");
+            if (!headRevision.equals(existingRevision)) {
+                log.info("Updating {} from {} revision to {}", expectedTagName, existingRevision, headRevision);
+                git.updateTag(expectedTagName, LogLevel.INFO);
+            } else {
+                log.debug("{} already matches head revision", expectedTagName, headRevision);
+            }
+        }
+    }
+}
