35b4ba6fb74c12beb35be1d99e8f3c4004d88ce0
Damien Biggs <dbiggs@vmware.com>
2017-11-08 11:12:36 -0500

    CodeCleanup: More code cleanup
    .

--- core/src/main/java/com/vmware/action/review/CopyReviewUrlToClipboard.java
+++ core/src/main/java/com/vmware/action/review/CopyReviewUrlToClipboard.java
@@ -3,14 +3,13 @@ package com.vmware.action.review;
 import com.vmware.action.base.BaseCommitUsingReviewBoardAction;
 import com.vmware.config.ActionDescription;
 import com.vmware.config.WorkflowConfig;
-import com.vmware.util.IOUtils;
+import com.vmware.util.CommandLineUtils;
 import com.vmware.util.UrlUtils;
-import com.vmware.util.exception.RuntimeIOException;
+import com.vmware.util.logging.LogLevel;
 
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
-import java.io.IOException;
 import java.util.Locale;
 
 @ActionDescription("Copies the review board url to the clipboard. Handy for pasting it into a browser.")
@@ -27,27 +26,14 @@ public class CopyReviewUrlToClipboard extends BaseCommitUsingReviewBoardAction {
         String osName = System.getProperty("os.name").toLowerCase(Locale.ENGLISH);
         boolean isOsx = osName.contains("mac") || osName.contains("darwin");
 
-        if (isOsx) {
-            copyUsingPbcopyCommand(reviewUrl);
+        if (isOsx && CommandLineUtils.isCommandAvailable("pbcopy")) {
+            log.debug("Using pbcopy command to copy review url to clipboard as it doesn't cause terminal in full screen mode to jump back to the desktop view");
+            CommandLineUtils.executeCommand(null, "pbcopy", reviewUrl, LogLevel.DEBUG);
         } else {
             StringSelection stringSelection = new StringSelection(reviewUrl);
             Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
             clpbrd.setContents(stringSelection, null);
         }
         log.info("Copied review url to clipboard");
-    }
-
-    private void copyUsingPbcopyCommand(String reviewUrl) {
-        log.debug("Using pbcopy command to copy review url to clipboard as it doesn't cause terminal in full screen mode to jump back to the desktop view");
-        ProcessBuilder builder = new ProcessBuilder("pbcopy").redirectErrorStream(true);
-        Process statusProcess;
-        try {
-            statusProcess = builder.start();
-        } catch (IOException e) {
-            throw new RuntimeIOException(e);
-        }
-        IOUtils.write(statusProcess.getOutputStream(), reviewUrl);
-        String output = IOUtils.read(statusProcess.getInputStream());
-        log.debug(output);
     }
 }
