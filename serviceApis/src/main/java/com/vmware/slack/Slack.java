6ed0b5f87626cb1068795a402c2b6f3eab4680e0
Damien Biggs <dbiggs@vmware.com>
2020-03-23 16:37:54 -0400

    Slack: Remove tokens
    Accidentally submitted.
    
    
    Former-commit-id: 5dccc45d9e8bc79f8124ade4ca706cff17b6b256

--- serviceApis/src/main/java/com/vmware/slack/Slack.java
+++ serviceApis/src/main/java/com/vmware/slack/Slack.java
@@ -17,8 +17,7 @@ public class Slack extends AbstractRestService {
         super(url, "/api", ApiAuthentication.none, username);
         this.connection = new HttpConnection(RequestBodyHandling.AsStringJsonEntity,
                 new ConfiguredGsonBuilder().namingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).build());
-        this.connection.addStatefulParam(RequestHeader.aBearerAuthHeader("xoxp-7985532022-10677794721-956768831555-bf19feadf00192d278f8abb84cae19a4"));
-        //this.connection.addStatefulParam(RequestHeader.aBearerAuthHeader("xoxb-7985532022-968583689670-Wa3NVsVoumuefF5TesHjxxxC"));
+        this.connection.addStatefulParam(RequestHeader.aBearerAuthHeader(""));
     }
 
     public void sendMessage(String channel, String text) {
