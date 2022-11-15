57d71376cabcb68500e5737ddb2751363e6a5a23
Damien Biggs <dbiggs@vmware.com>
2022-11-15 10:54:04 -0500

    UnitTest: Remove unneeded unit test
    .

--- utils/src/test/java/com/vmware/utils/TestArrayUtils.java
+++ utils/src/test/java/com/vmware/utils/TestArrayUtils.java
@@ -1,20 +1,12 @@
 package com.vmware.utils;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import com.vmware.util.ArrayUtils;
+import org.junit.Test;
+
 import java.util.Arrays;
-import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Stream;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.vmware.util.ArrayUtils;
-import com.vmware.util.IOUtils;
-import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -53,15 +45,5 @@ public class TestArrayUtils {
         assertEquals(-3, Arrays.binarySearch(values, 37));
         assertEquals(2, Arrays.binarySearch(values, 56));
         assertEquals(-4, Arrays.binarySearch(values, 600));
-    }
-
-    @Test
-    public void updateJwfToken() throws IOException {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        Map<String, String> stfConfig = gson.fromJson(new FileReader("/Users/dbiggs/cloud/dev-integration/system-tests/SystemTests/nsxTGlobalTest.json"), Map.class);
-        String jwtToken = IOUtils.read(new File("/Users/dbiggs/.development-org-vcd-api-token.txt"));
-        stfConfig.put("jwtToken", jwtToken);
-        IOUtils.write(new File("/Users/dbiggs/cloud/dev-integration/system-tests/SystemTests/nsxTGlobalTest.json"), gson.toJson(stfConfig));
-        IOUtils.write(new File("/Users/dbiggs/cloud/dev-integration/system-tests/SystemTests/populated-bindings.properties"), "{}");
     }
 }
