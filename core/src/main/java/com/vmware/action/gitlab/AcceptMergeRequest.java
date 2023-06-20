13e832d653e316dde709621625b9752635781eb4
Damien Biggs <dbiggs@vmware.com>
2023-06-20 10:41:01 -0400

    MergeRequest: Always try to accept merge request
    CanBeMerged can be irrelevant.

--- core/src/main/java/com/vmware/action/gitlab/AcceptMergeRequest.java
+++ core/src/main/java/com/vmware/action/gitlab/AcceptMergeRequest.java
@@ -4,7 +4,6 @@ import com.vmware.action.base.BaseCommitWithMergeRequestAction;
 import com.vmware.config.ActionDescription;
 import com.vmware.config.WorkflowConfig;
 import com.vmware.gitlab.domain.MergeRequest;
-import com.vmware.util.exception.FatalException;
 
 @ActionDescription("Accept merge request in Gitlab so that it is merged to the target branch.")
 public class AcceptMergeRequest extends BaseCommitWithMergeRequestAction {
@@ -16,10 +15,6 @@ public class AcceptMergeRequest extends BaseCommitWithMergeRequestAction {
     public void process() {
         log.info("Accepting merge request {}", draft.mergeRequestUrl);
         MergeRequest mergeRequest = draft.getGitlabMergeRequest();
-        if (!mergeRequest.canBeMerged()) {
-            throw new FatalException("Cannot accept merge request {} as it has a status of {} and cannot be merged",
-                    mergeRequest.iid, mergeRequest.mergeStatus);
-        }
         gitlab.acceptMergeRequest(mergeRequest);
     }
 }
